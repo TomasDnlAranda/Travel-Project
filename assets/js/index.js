b13c12ce48b567ad77ee977c15ddd8d75474536c
AxelAngelucci <axeljesusangelucci@gmail.com>
2022-01-23 23:12:55 -0300

    Packages section added

--- assets/js/index.js
+++ assets/js/index.js
@@ -37,3 +37,38 @@ videobtn.forEach(btn =>{
         document.querySelector('#video-slider').src = src
     });
 });
+
+
+var swiper = new Swiper('.swiper-container', {
+	navigation: {
+	  nextEl: '.swipe-right',
+	  prevEl: '.swipe-left'
+	},
+	slidesPerView: 1,
+	spaceBetween: 10,
+	// init: false,
+	pagination: {
+	  el: '.swiper-pagination',
+	  clickable: true,
+	},
+
+  
+	breakpoints: {
+	  620: {
+		slidesPerView: 1,
+		spaceBetween: 20,
+	  },
+	  680: {
+		slidesPerView: 2,
+		spaceBetween: 40,
+	  },
+	  920: {
+		slidesPerView: 3,
+		spaceBetween: 40,
+	  },
+	  1240: {
+		slidesPerView: 3,
+		spaceBetween: 50,
+	  },
+	} 
+    });
